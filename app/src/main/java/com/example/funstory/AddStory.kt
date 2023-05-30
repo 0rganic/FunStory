965425228d9e6db0a8e86ea43f22f3a52cfd9e4d
0rganic <jeremydiazp@gmail.com>
2023-05-31 02:20:28 +0700

    first commit

---
+++ app/src/main/java/com/example/funstory/AddStory.kt
@@ -0,0 +1,11 @@
+package com.example.funstory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddStory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_story)
+    }
+}
