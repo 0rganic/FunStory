5df702b30d67aa68b7e3be6310f7c86fe40a0390
0rganic <jeremydiazp@gmail.com>
2023-05-31 16:45:56 +0700

    layouting done

--- app/src/main/java/com/example/funstory/AddStory.kt
+++ app/src/main/java/com/example/funstory/AddStory.kt
@@ -7,5 +7,8 @@ class AddStory : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_story)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title ="New Story"
     }
 }
--- app/src/main/java/com/example/funstory/Camera.kt
+++ app/src/main/java/com/example/funstory/Camera.kt
@@ -7,5 +7,7 @@ class Camera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        supportActionBar!!.hide()
     }
 }
